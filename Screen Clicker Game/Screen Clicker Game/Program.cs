1c4a9536f88998de9059ddd6ece2f93f3eaf4a9b
Wiles_Joshua <Wiles_Joshua@edu.mcctc.net>
2020-09-24 10:56:54 -0400

    File

---
+++ Screen Clicker Game/Screen Clicker Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screen_Clicker_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
