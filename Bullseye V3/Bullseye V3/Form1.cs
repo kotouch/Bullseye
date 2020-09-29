f5823e8c0e8d5e8460924e29397785f638149498
kingjosh0615 <54904560+kingjosh0615@users.noreply.github.com>
2020-09-29 16:33:05 -0400

    New File

---
+++ Bullseye V3/Bullseye V3/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bullseye_V3
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void UpdateBoxOne(string message)
+        {
+            textBox1.Text = message;
+        }
+
+        public static void UpdateBoxTwo(string message)
+        {
+
+            textBox2.Text = message;
+        }
+    }
+}
---
+++ Bullseye V3/Bullseye V3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Bullseye_V3
+{
+     static class Program
+    {
+        public static int playerX;
+        public static int playerY;
+        
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+         static void Main()
+        {
+            Form1 form1 = new Form1();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);  
+            Application.Run(form1);
+            int x = 0;
+            while (true)
+            {
+                playerX = Cursor.Position.X;
+                playerY = Cursor.Position.Y;
+
+                Console.WriteLine($"The x of your cursor is: {playerX}.");
+                Console.WriteLine($"The y of your cursor is: {playerY}.");
+                Form1.UpdateBoxOne($"The x of your cursor is: {playerX}.");
+
+                x++;
+                if (x % 50 == 0)
+                {
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
